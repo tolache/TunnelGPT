df2a9ad906c81ed8b8e1ed530be8d831272f8876
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2025-01-05 12:20:42 +0100

    Refactor API Gateway config

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,7 +1,4 @@
-﻿data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
-data "aws_lambda_function" "lambda" {
+﻿data "aws_lambda_function" "lambda" {
   function_name = "TunnelGPT"
 }
 
@@ -28,7 +25,7 @@ resource "aws_api_gateway_integration" "lambda" {
   http_method             = aws_api_gateway_method.any_method.http_method
   integration_http_method = "POST"
   type                    = "AWS"
-  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:TunnelGPT/invocations"
+  uri                     = data.aws_lambda_function.lambda.invoke_arn
 
   passthrough_behavior = "WHEN_NO_MATCH"
   content_handling     = "CONVERT_TO_TEXT"
@@ -79,9 +76,11 @@ resource "aws_api_gateway_stage" "prod" {
 }
 
 resource "aws_lambda_permission" "api_gateway" {
+  depends_on = [aws_api_gateway_deployment.api_deployment]
+  
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = data.aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
