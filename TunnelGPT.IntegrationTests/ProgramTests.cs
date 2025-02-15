1beaae2bab64b44d27d2a3182e63a1c9dc4af1e9
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2025-02-15 14:40:01 +0100

    Add tests

--- TunnelGPT.IntegrationTests/ProgramTests.cs
+++ TunnelGPT.IntegrationTests/ProgramTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot.Types;
+using TunnelGPT.Infrastructure.Configuration;
 
 namespace TunnelGPT.IntegrationTests;
 
@@ -19,14 +24,100 @@ public class ProgramTests(WebApplicationFactory<Program> factory) : IClassFixtur
     }
 
     [Fact]
-    public async Task PostWithoutSecret_ReturnsUnauthorized()
+    public async Task PostWithCorrectSecretAndData_ReturnsOk()
     {
-        throw new NotImplementedException();
+        // Arrange
+        JsonContent payload = JsonContent.Create(new Update());
+        string telegramBotSecret = factory.Server.Services.GetRequiredService<AppSettings>().TelegramBotSecret;
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("/", UriKind.Relative),
+            Headers =
+            {
+                {HttpRequestHeader.ContentType.ToString(), "application/json"},
+                {"X-Telegram-Bot-Api-Secret-Token", telegramBotSecret}
+            },
+            Content = payload,
+        };
+        
+        // Act
+        HttpResponseMessage response = await _client.SendAsync(request);
+        
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 
     [Fact]
-    public async Task PostWithIvalidPayload_ReturnsBadRequest()
+    public async Task PostWithoutSecret_ReturnsUnauthorized()
     {
-        throw new NotImplementedException();
+        // Arrange
+        JsonContent payload = JsonContent.Create(new Update());
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("/", UriKind.Relative),
+            Headers =
+            {
+                {HttpRequestHeader.ContentType.ToString(), "application/json"},
+            },
+            Content = payload,
+        };
+        
+        // Act
+        HttpResponseMessage response = await _client.SendAsync(request);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task PostWitIncorrectSecret_ReturnsUnauthorized()
+    {
+        // Arrange
+        JsonContent payload = JsonContent.Create(new Update());
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("/", UriKind.Relative),
+            Headers =
+            {
+                {HttpRequestHeader.ContentType.ToString(), "application/json"},
+                {"X-Telegram-Bot-Api-Secret-Token", "incorrect_value"},
+            },
+            Content = payload,
+        };
+        
+        // Act
+        HttpResponseMessage response = await _client.PostAsync("/", payload);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostWithInvalidPayload_ReturnsBadRequest()
+    {
+        // Arrange
+        JsonContent payload = JsonContent.Create("");
+        string telegramBotSecret = factory.Server.Services.GetRequiredService<AppSettings>().TelegramBotSecret;
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("/", UriKind.Relative),
+            Headers =
+            {
+                {HttpRequestHeader.ContentType.ToString(), "application/json"},
+                {"X-Telegram-Bot-Api-Secret-Token", telegramBotSecret}
+            },
+            Content = payload,
+        };
+        
+        // Act
+        HttpResponseMessage response = await _client.SendAsync(request);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
