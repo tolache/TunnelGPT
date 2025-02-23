be4add669a9b9504ea34d65e6c27a8a735d594e7
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2025-02-23 11:59:39 +0100

    update deployment script to add firewall rules

--- .teamcity/install-tunnelgpt.sh
+++ .teamcity/install-tunnelgpt.sh
@@ -32,6 +32,17 @@ chmod 600 $application_home/appsettings*.json $application_home/tunnelgpt-cert.*
 # Allow dotnet to bind to well-known ports (required if environment is Production and application user is non-root)
 setcap CAP_NET_BIND_SERVICE=+eip "$(readlink -f /usr/bin/dotnet)"
 
+# Add firewall rules
+for port in 80 443; do
+  if iptables -C INPUT -m state --state NEW -p tcp --dport $port -j ACCEPT 2>/dev/null; then
+    echo "A rule allowing port $port already exists. No changes made.";
+  else
+    echo "No rule found for port $port. Adding rule...";
+    iptables -I INPUT 6 -m state --state NEW -p tcp --dport $port -j ACCEPT
+    netfilter-persistent save
+  fi
+done
+
 # Register service
 tee /etc/systemd/system/tunnelgpt.service > /dev/null <<EOF
 [Unit]
