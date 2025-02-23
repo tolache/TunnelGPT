171642ab7963914dd722617f91533a355109acb7
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2025-02-23 08:36:31 +0100

    update CI settings

--- .teamcity/create-cert.ps1
+++ .teamcity/create-cert.ps1
@@ -7,12 +7,18 @@ param(
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+$certPemName = "./publish/tunnelgpt-cert.pem"
+$certKeyName = "./publish/tunnelgpt-cert.key"
+
+
 try {
     openssl req -x509 -newkey rsa:4096 -sha256 -nodes -days 36500 `
-        -keyout ./publish/tunnelgpt-cert.key `
-        -out ./publish/tunnelgpt-cert.pem `
+        -keyout $certKeyName `
+        -out $certPemName `
         -subj "/C=NL/O=tolache/CN=$Servername" `
         -addext "subjectAltName=IP:$Servername"
+
+    dos2unix $certKeyName $certPemName
 }
 catch {
     Write-Error "Failed to generate certificate. Reason:`n$_"
